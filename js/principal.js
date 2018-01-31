d078ab1aaa5b2206f635758dc515f1b6fd1f55c5
KaryRQ <darkness.kary@hotmail.com>
2018-01-30 20:49:47 -0600

    pinta una imagen yeih

--- js/principal.js
+++ js/principal.js
@@ -1,34 +1,11 @@
-$(document).ready(function(){
- 
-    $('input[type=file]').change(function(){
- 
-        $(this).simpleUpload("/ajax/upload.php", {
- 
-            start: function(file){
-                //upload started 
-                $('#filename').html(file.name);
-                $('#progress').html("");
-                $('#progressBar').width(0);
-            },
- 
-            progress: function(progress){
-                //received progress 
-                $('#progress').html("Progress: " + Math.round(progress) + "%");
-                $('#progressBar').width(progress + "%");
-            },
- 
-            success: function(data){
-                //upload successful 
-                $('#progress').html("Success!<br>Data: " + JSON.stringify(data));
-            },
- 
-            error: function(error){
-                //upload failed 
-                $('#progress').html("Failure!<br>" + error.name + ": " + error.message);
-            }
- 
-        });
- 
-    });
- 
-});
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var dataURL = reader.result;
+      var output = document.getElementById('output');
+      output.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
+  };
